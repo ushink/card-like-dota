a1d354ed986515f4a943d8173b08dcd4f13d5e7d
Ulyana <ushinkevich@mail.ru>
2024-05-30 16:25:01 +0400

    pages

---
+++ src/pages/Hero/Hero.tsx
@@ -0,0 +1,9 @@
+import s from "./Hero.module.css";
+
+export function Hero() {
+  return (
+    <div>
+      <h1 className={s.title}>Heroes</h1>
+    </div>
+  );
+}
---
+++ src/pages/Heroes/Heroes.tsx
@@ -0,0 +1,9 @@
+import s from "./Heroes.module.css";
+
+export function Heroes() {
+  return (
+    <div>
+      <h1 className={s.title}>Heroes</h1>
+    </div>
+  );
+}
