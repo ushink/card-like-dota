b3121bacafaa70decd8d213db8d011b9ca51afc7
Ulyana <ushinkevich@mail.ru>
2024-06-02 15:53:10 +0400

    data is ok

--- src/pages/Hero/Hero.tsx
+++ src/pages/Hero/Hero.tsx
@@ -1,9 +1,11 @@
+import { useParams } from "react-router-dom";
 import s from "./Hero.module.css";
 
 export function Hero() {
+  const { id } = useParams();
   return (
     <div>
-      <h1 className={s.title}>Heroes</h1>
+      <h1 className={s.title}>Hero id = {id}</h1>
     </div>
   );
 }
--- src/pages/Heroes/Heroes.tsx
+++ src/pages/Heroes/Heroes.tsx
@@ -1,9 +1,26 @@
+import { Link } from "react-router-dom";
+import { useGetHeroesStatsQuery } from "../../services/heroes";
 import s from "./Heroes.module.css";
 
 export function Heroes() {
+  const { data } = useGetHeroesStatsQuery();
+  console.log(data);
   return (
     <div>
       <h1 className={s.title}>Heroes</h1>
+
+      {data?.map((el) => (
+        <div key={el.id}>
+          <Link to={`/hero/${el.id}`}>
+            <h1>{el.localized_name}</h1>
+          </Link>
+
+          <img
+            src={`https://cdn.cloudflare.steamstatic.com/${el.img}`}
+            alt={el.name}
+          />
+        </div>
+      ))}
     </div>
   );
 }
