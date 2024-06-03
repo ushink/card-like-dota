801729137315a83aca31856f0ab2e149ea1563f4
Ulyana <ushinkevich@mail.ru>
2024-06-03 20:30:37 +0400

    persist redux (err)

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,16 +1,39 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { dotaApi } from "../services/heroes";
-import dotaSlice from "./dotaSlice";
+import dotaReducer from "./dotaSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const rootReducer = combineReducers({
+  dota: dotaReducer,
+  [dotaApi.reducerPath]: dotaApi.reducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer); //TODO: не работает сохранение в store
 
 export const store = configureStore({
-  reducer: {
-    dota: dotaSlice,
-    [dotaApi.reducerPath]: dotaApi.reducer, // TODO: оставить или убрать?
-  },
+  reducer: persistedReducer,
 
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(dotaApi.middleware),
+    getDefaultMiddleware({
+      serializableCheck: {
+        // Игнорировать эти типы действий
+        ignoredActions: ["persist/PERSIST"],
+        // Игнорировать эти пути полей во всех действиях
+        ignoredActionPaths: ["meta.arg", "payload.timestamp"],
+        // Игнорировать эти пути в состоянии
+        ignoredPaths: ["items.dates"],
+      },
+    }).concat(dotaApi.middleware),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
